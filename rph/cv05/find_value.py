b791e30e5ca2b2f4c4149af47ab0055e945738fe
maresj29 <maresj29@fel.cvut.cz>
2016-11-10 14:53:12 +0100

    rph cv06

--- rph/cv05/find_value.py
+++ rph/cv05/find_value.py
@@ -18,3 +18,8 @@ def value_positions(data, value):
 
     return coordinates
 
+if __name__ == "__main__":
+    seznam = [[1,2,3],[4,2,3],[7,3,1],[7,8,9],[0,7,5,2,6,7,8,2,4,6,1,2,3,3,8,9,6]]
+
+    print (value_count(seznam, 23))
+    print (value_positions(seznam, 1))
--- rph/reversi/player.py
+++ rph/reversi/player.py
@@ -3,6 +3,17 @@ class MyPlayer:
 
     def __init__(self, my_color, opponent_color):
         self.name = "maresj29"
+        self.my_color = my_color
+        self.opponent_color = opponent_color
 
     def move(self, board):
-        return (1, 2)
+        get_valid_moves(board)
+
+    def get_valid_moves(self, board):
+        pass
+
+    def search_around(self, board):
+        pass
+        #podle vysledku zavolat search in direction
+
+    def search_in_direction(self, board):
